b308921de4da0bfbe783e60cc1e13370ebc7f364
Vaishnavi Rathi <vaishnavi.rathi@anchanto.com>
2021-10-13 15:49:16 +0530

    users created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,55 +1,50 @@
-class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+class PostsController <ApplicationController
     def show
-        
+      #byebug
+      @post=Post.find(params[:id])
+     # @comments= @article.comments.order('comments.created_at DESC')
     end
-
+  
     def index
-        @posts = Post.all
+     # @posts =Post.paginate(page: params[:page], per_page: 5)
+     @posts= Post.all
     end
-
+  
     def new
-        @post = Post.new
+      @post= Post.new
     end
-
-    def edit
     
+    def edit
+      @post = Post.find(params[:id])
     end
-
+  
     def create
-        @post = Post.new(post_params)
-        if @post.save
-            flash[:notice] = "Post was created successfully."
-            redirect_to @post
-          else
-            render 'new'
-          end
+      #render plain: params[:article]
+      @post= Post.new(params.require(:post).permit(:title, :description))
+      @post.user=current_user
+      #render plain: @article
+      #render plain: @article.inspect
+        if @post.save   #redirect_to article_path(@article)
+           redirect_to @post
+        else
+           render 'new'
+        end
     end
-
+  
     def update
-        @post = Post.find(params[:id])
-        if @post.update(params.require(:post).permit(:title, :description))
-          flash[:notice] = "Post was updated successfully."
-          redirect_to @post
+      @post = Post.find(params[:id])
+        if  @post.update(params.require(:post).permit(:title, :description))
+         #flash[:notice] = "Article was updated successfully."
+         redirect_to @post
         else
           render 'edit'
         end
-      end
-      
-      def destroy
-        @post.destroy
-        redirect_to posts_path
-      end
-
-        private
-        def set_post
-            @post = Post.find(params[:id])
-        end
-        
-        def post_params
-            params.require(:post).permit(:title, :description)
-        end
-
     end
-
+  
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to posts_path 
+   end
+   
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about' , to: 'pages#about'
   resources :posts
+  get'signup' ,to: 'users#new'
+  #resources :users, except: [:new]
+  resources :users , only: [:show ,:index]
+  #resources :comments, only: [:new,:create]
 
 end
 
